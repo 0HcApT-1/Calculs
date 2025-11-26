f30b363556df87ea59874aa777691e6a690ad46f
hk93190 <hkbousdiouf@gmail.com>
2025-11-26 19:42:06 +0100

    Eviter une répétition de code.

--- Calculs/Program.cs
+++ Calculs/Program.cs
@@ -11,12 +11,13 @@ namespace Calculs
         {
             // variables 
             Random rand = new Random(); // outil de génération de nombre aléatoire
-            int val1, val2; // mémorisation de nombres aléatoires
+             // mémorisation de nombres aléatoires
             int solution; // calcul de la solution
             int reponse = 0; // saisie de la réponse de l'utilisateur
             string choix; // saisie du choix de l'utilsiateur
             bool correct; // mémorise si la saisie est un entier
-
+            int val1 = rand.Next(1, 10), val2 = rand.Next(1, 10);
+            
             // boucle sur le menu
             choix = "1";
             while (choix != "0")
@@ -31,8 +32,7 @@ namespace Calculs
                 switch (choix)
                 {
                     case "1": // addition
-                        val1 = rand.Next(1, 10);
-                        val2 = rand.Next(1, 10);
+                        
                         // saisie de la réponse
                         correct = false;
                         while (!correct)
